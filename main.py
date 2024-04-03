eee85d8f53a7dcc45169e81e1525bf946e568ed5
Timofey <vash.banan@gmail.com>
2024-04-03 22:27:53 +0300

    чето

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-if __name__ == "__main__":
-    print("correct")
+from typing import Final
+
+TOKEN: Final = '7045425968:AAFXlF9K-5GXLiO_hbWKmQIP-mG0YXGb6vk'
+BOT_USERNAME : Final = '@From_video_to_gif_bot'
+
